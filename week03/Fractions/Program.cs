60b2ef2c3d6594ffbb42e8e251e4cefc44dac0a6
JKs162426 <jesus20jk@gmail.com>
2025-01-21 22:40:05 -0700

    Activity Completed

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction fraction = new Fraction(2,8);
+        fraction.GetTop();
+        fraction.GetBottom();
+
+        string stringValue = fraction.GetFractionString();
+        Console.WriteLine(stringValue);
+        double decimalValue = fraction.GetDecimalValue();
+        Console.WriteLine(decimalValue);
+
     }
 }
