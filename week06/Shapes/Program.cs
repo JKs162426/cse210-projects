c18434f1575f56df0f2a16c7b4dfe0893b73f0f1
JKs162426 <jesus20jk@gmail.com>
2025-02-11 19:54:23 -0700

    Shapes Activity Completed

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+        Console.Clear();
+
+        Square square = new Square();
+        square.SetColor("Orange");
+        square.SetSide(8);
+        square.GetArea();
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.SetColor("Red");
+        rectangle.SetLenght(5.5);
+        rectangle.SetWidth(7);
+
+        Circle circle = new Circle();
+        circle.SetColor("Pink");
+        circle.SetRadius(15.8);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape sh in shapes)
+        {   
+            string color = sh.GetColor();
+            Console.WriteLine($"Color {color} and Area {sh.GetArea()}");
+        }
     }
 }
---
+++ week06/Shapes/Rectangle.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Rectangle : Shape
+{
+    private double _lenght;
+    private double _width;
+
+    public double GetLenght()
+    {
+        return _lenght;
+    }
+
+    public void SetLenght(double lenght)
+    {
+        _lenght = lenght;
+    }
+
+    public double GetWidth()
+    {
+        return _width;
+    }
+
+    public void SetWidth(double width)
+    {
+        _width = width;
+    }
+
+    public override double GetArea()
+    {
+        return _lenght * _width;
+    }
+}
